b6f2a70a8fd2f1aab3e8ca9929f08f954f92dbad
valirshad <irshad@xelpmoc.in>
2018-01-26 00:08:39 +0530

    new file added

---
+++ app/src/main/java/com/irshad/practice/adapter/DrawerItemCustomAdapter.java
@@ -0,0 +1,50 @@
+package com.irshad.practice.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.irshad.practice.R;
+import com.irshad.practice.model.DataModel;
+
+
+
+public class DrawerItemCustomAdapter extends ArrayAdapter<DataModel> {
+
+    Activity activity;
+    int layoutResourceId;
+    DataModel data[] = null;
+
+    public DrawerItemCustomAdapter(Activity activity, int layoutResourceId, DataModel[] data) {
+
+        super(activity, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.activity = activity;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItem = convertView;
+
+        LayoutInflater inflater = ((Activity) activity).getLayoutInflater();
+        listItem = inflater.inflate(layoutResourceId, parent, false);
+
+        ImageView imageViewIcon = (ImageView) listItem.findViewById(R.id.imageViewIcon);
+        TextView textViewName = (TextView) listItem.findViewById(R.id.textViewName);
+
+        DataModel folder = data[position];
+
+
+       // imageViewIcon.setImageResource(folder.icon);
+        textViewName.setText(folder.name);
+
+        return listItem;
+    }
+}
---
+++ app/src/main/java/com/irshad/practice/model/DataModel.java
@@ -0,0 +1,14 @@
+package com.irshad.practice.model;
+
+
+public class DataModel {
+   // public int icon;
+    public String name;
+
+    // Constructor.
+    public DataModel(String name) {
+
+
+        this.name = name;
+    }
+}
